07df144d4a9919dc4909ae628486ce2e545391fa
ravipatelsimform <ravi.patel@simformsolutions.com>
2023-03-24 10:51:06 +0530

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'standard',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {}
+}
