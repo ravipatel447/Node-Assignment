e176fe695e9c989f5545803158d1e7cf14de5c61
ravipatelsimform <ravi.patel@simformsolutions.com>
2023-03-24 12:09:21 +0530

    ejs commit

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
+const path = require('path')
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/home', (req, res) => {
+  res.render('index')
+})
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+app.get('/services', (req, res) => {
+  res.render('services')
+})
+app.get('/categories', (req, res) => {
+  res.render('categories')
+})
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+app.get('/', (req, res) => {
+  res.render('index')
+})
 app.listen(PORT, () => {
   console.log(`we are listing at port ${PORT}`)
 })
